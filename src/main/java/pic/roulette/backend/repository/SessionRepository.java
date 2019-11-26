00ee0634f1948390bf92aa0bcfc96cc37e3e573e
hello57748 <sampeters@csumb.edu>
2019-11-26 14:18:42 -0800

    heroku api testing

--- src/main/java/pic/roulette/backend/repository/SessionRepository.java
+++ src/main/java/pic/roulette/backend/repository/SessionRepository.java
@@ -1,7 +1,6 @@
 package pic.roulette.backend.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import pic.roulette.backend.model.Session;
 
 @Repository
